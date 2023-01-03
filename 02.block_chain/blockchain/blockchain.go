02f5c17b2168ef3b5896998e98a67daa2c072a23
formegusto <formegusto@gmail.com>
2023-01-03 17:32:59 +0900

    SingleTone : Block Generate, Sync.Once.Do

--- 02.block_chain/blockchain/blockchain.go
+++ 02.block_chain/blockchain/blockchain.go
@@ -1,5 +1,11 @@
 package blockchain
 
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
 type block struct {
 	data 		string
 	hash 		string
@@ -7,16 +13,42 @@ type block struct {
 }
 
 type blockchain struct {
-	blocks []block
+	blocks []*block
 }
 
 // 1. singletone variable
 var b *blockchain
+var once sync.Once
 
 // 2. control function
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.data + b.prevHash))
+	b.hash = fmt.Sprintf("%x",hash)
+}
+
+func getLastHash() string {
+	totalBlock := len(GetBlockchain().blocks)
+	if totalBlock == 0 {
+		return ""
+	}
+	return GetBlockchain().blocks[totalBlock - 1].hash
+}
+
+func createBlock(data string) *block {
+	newBlock := block{data, "", getLastHash()}
+	newBlock.calculateHash()
+
+	return &newBlock
+}
+
 func GetBlockchain() *blockchain {
 	if b == nil {
-		b = &blockchain{}
+		once.Do(func() {
+			b = &blockchain{}
+			b.blocks = append(b.blocks, createBlock("GenesisBlock"))
+		})
 	}
 	return b
 }
+
+
