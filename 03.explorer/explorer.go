f350ca322ea733d79b26c0a0940712be46c569ac
formegusto <formegusto@gmail.com>
2023-01-04 18:27:35 +0900

    add refactoring

--- 03.explorer/explorer.go
+++ 03.explorer/explorer.go
@@ -1,75 +1,81 @@
 package explorer_test
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"text/template"
+import "github.com/formegusto/study-go-chain/03.explorer/explorer"
 
-	"github.com/formegusto/study-go-chain/02.block_chain/blockchain"
-)
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"text/template"
 
-const (
-	port string = ":4000"
-	templateDir string = "templates/"
-)
-var templates *template.Template
+// 	"github.com/formegusto/study-go-chain/02.block_chain/blockchain"
+// )
 
-type homeData struct {
-	// template에서 공우되는 것 마저,, 대문자로 공유해준다!
-	PageTitle string
-	Blocks []*blockchain.Block
-}
+// const (
+// 	port string = ":4000"
+// 	templateDir string = "templates/"
+// )
+// var templates *template.Template
 
-// 2개의 인자를 받는다.
-// 1. http.ResponseWriter : 유저에게 보내고 싶은 데이터 정의
-// 2. *http.Request : request로는 큰 파일이 올 수도 있기 때문에 복사보다는 실제 값을 이용하는 것을 지향한다.
-func home(rw http.ResponseWriter, r *http.Request) {
-	// console에 출력이 아닌, Writer(출력 스트림)에 출력하는 기능
-	// fmt.Fprint(rw, "Hello from home!")
-	
-	// * templates
-	// tmpl := template.Must(template.ParseFiles("templates/pages/home.gohtml"))
+// type homeData struct {
+// 	// template에서 공유되는 것 마저,, 대문자로 공유해준다!
+// 	PageTitle string
+// 	Blocks []*blockchain.Block
+// }
 
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// Must Function을 사용해주면 위에서 작업한 error를 모듈에서 알아서 처리해준다.
+// // 2개의 인자를 받는다.
+// // 1. http.ResponseWriter : 유저에게 보내고 싶은 데이터 정의
+// // 2. *http.Request : request로는 큰 파일이 올 수도 있기 때문에 복사보다는 실제 값을 이용하는 것을 지향한다.
+// func home(rw http.ResponseWriter, r *http.Request) {
+// 	// console에 출력이 아닌, Writer(출력 스트림)에 출력하는 기능
+// 	// fmt.Fprint(rw, "Hello from home!")
 
-	// Writer, data 를 요구한다.
-	// Template 에서 받고자 하는 데이터를 정의해준다.
-	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	templates.ExecuteTemplate(rw, "home",data)
-}
+// 	// * templates
+// 	// tmpl := template.Must(template.ParseFiles("templates/pages/home.gohtml"))
 
-func add(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-		case "GET":
-			templates.ExecuteTemplate(rw, "add", nil)
-		case "POST":
-			// r.Form이 생성됨
-			r.ParseForm()
+// 	// if err != nil {
+// 	// 	log.Fatal(err)
+// 	// }
+// 	// Must Function을 사용해주면 위에서 작업한 error를 모듈에서 알아서 처리해준다.
 
-			// block을 chain에 추가
-			data := r.Form.Get("blockData")
-			blockchain.GetBlockchain().AddBlock(data)
+// 	// Writer, data 를 요구한다.
+// 	// Template 에서 받고자 하는 데이터를 정의해준다.
+// 	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+// 	templates.ExecuteTemplate(rw, "home",data)
+// }
 
-			http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
-	}
-}
+// func add(rw http.ResponseWriter, r *http.Request) {
+// 	switch r.Method {
+// 		case "GET":
+// 			templates.ExecuteTemplate(rw, "add", nil)
+// 		case "POST":
+// 			// r.Form이 생성됨
+// 			r.ParseForm()
+
+// 			// block을 chain에 추가
+// 			data := r.Form.Get("blockData")
+// 			blockchain.GetBlockchain().AddBlock(data)
+
+// 			http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+// 	}
+// }
+
+// func Open() {
+// 	// template preload
+// 	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+// 	// templates 를 쓰는 것이 중요하다. 빈 곳에 저장 후 추가 저장한다는 느낌임
+// 	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+
+// 	// routes
+// 	http.HandleFunc("/", home)
+// 	http.HandleFunc("/add", add)
+
+// 	fmt.Printf("Listening on http://localhost%s\n", port)
+
+// 	// 에러가 발생하면 error 출력 후 os.Exit(1) 으로 프로그램 종료
+// 	log.Fatal(http.ListenAndServe(port, nil))
+// }
 
 func Open() {
-	// template preload
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	// templates 를 쓰는 것이 중요하다. 빈 곳에 저장 후 추가 저장한다는 느낌임
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-
-	// routes
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-
-	fmt.Printf("Listening on http://localhost%s\n", port)
-
-	// 에러가 발생하면 error 출력 후 os.Exit(1) 으로 프로그램 종료
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
 }
