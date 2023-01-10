4e35cd0998f55c4704a53a2be6982263820ce628
formegusto <formegusto@gmail.com>
2023-01-10 18:05:55 +0900

    CLI : FlagSet Intro

--- 05.my_cli/my_cli.go
+++ 05.my_cli/my_cli.go
@@ -1,6 +1,7 @@
 package my_cli
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -25,15 +26,36 @@ func Test() {
 		welcome()
 	}
 
-	// option은 3번째 부터 붙는 걸로!
-	fmt.Println(params[2:])
+	/*
+	Handler Type
+	const (
+		ContinueOnError ErrorHandling = iota // Return a descriptive error.
+		ExitOnError                          // Call os.Exit(2) or for -h/-help Exit(0).
+		PanicOnError                         // Call panic with a descriptive error.
+	)
+	*/
+	// create flag set
+	flags := flag.NewFlagSet("flags", flag.ExitOnError)
+
+	// create flag item
+	// key, default value, error message 
+	// return *int 아,, 주소를 쓰는 이윸ㅋㅋ 밑에 붙음 parsed 할 때
+	portFlag := flags.Int("port", 4000, "Sets the port of the server")
 
 	switch params[1] {
 		case "explorer":
 			fmt.Println("Start Explorer")
 		case "rest":
 			fmt.Println("Start REST API")
+			// port를 찾고, port 값이 있는지, integer인지 체크하고 그에 따라 반응
+			flags.Parse(params[2:])
 		default:
 			welcome()
+	}
+
+	// parse가 제대로 되었을 때, true 반환
+	if flags.Parsed() {
+		fmt.Println(*portFlag)
+		fmt.Println("Start server")
 	}
 }
