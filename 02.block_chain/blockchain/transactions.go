95447db7a214d0175101dd20e4ec9f920130339e
formegusto <formegusto@gmail.com>
2023-01-20 10:07:50 +0900

    UTx : Before Refactoring

--- 02.block_chain/blockchain/transactions.go
+++ 02.block_chain/blockchain/transactions.go
@@ -15,11 +15,6 @@ type mempool struct {
 	Txs []*Tx 
 }
 
-// Blockchain의 b변수와는 다르게
-// 초기화가 필요없다. 데이터베이스에 넣지 않을 것 이기 때문이다.
-// 왜냐하면 transaction이 성립되면 mempool에서 transaction은 없어지고, 블록 내부로 들어와
-// 블록체인 DB에 상주하게 된다.
-// mempool은 메모리에서만 존재한다.
 var Mempool *mempool = &mempool{}
 
 type Tx struct {
@@ -33,11 +28,9 @@ func (tx *Tx) getId() {
 }
 
 type TxIn struct {
-	TxID	string	`json:"txID"`// 이전 Tx를 찾는 방법
-	Index	int		`json:"index"`// TxId의 Tx어디에 연결된 TxOut이 위치해 있는가
+	TxID	string	`json:"txID"`
+	Index	int		`json:"index"`
 	Owner 	string	`json:"owner"`
-	// 특정 Output으로 부터 받아올 것 이기 때문에 필요 없다.
-	// Amount 	int
 }
 
 type TxOut struct {
@@ -45,7 +38,6 @@ type TxOut struct {
 	Amount 	int
 }
 
-// unspent transaction output
 type UTxOut struct {
 	TxID	string	`json:"txID"`
 	Index	int		`json:"index"`
@@ -64,15 +56,10 @@ func isOnMempool(uTxOut *UTxOut) bool {
 	return exists
 }
 
-// address is miner address
 func makeCoinbaseTx(address string) *Tx {
-	// owner : 소유주
-	// amount : 채굴자에게 지급할 액수의 수량
 	txIns := []*TxIn {
 		{"", -1 , "COINBASE"},
 	}
-	// owner : 채굴자의 주소
-	// amount : 거래 총량
 	txOuts := []*TxOut {
 		{address, minerReward},
 	}
@@ -86,57 +73,6 @@ func makeCoinbaseTx(address string) *Tx {
 	return &tx
 }
 
-// // v1
-// func makeTx(from, to string, amount int) (*Tx, error) {
-// 	// 누군가가 얼마만큼 가지고 있는지 알고 싶다면 출력값을 참고해야 한다.
-// 	// transaction을 시작하고 싶다면 입력값을 만들면 된다.
-// 	// 그리고 입력값은 blockchain 가지고 있는 나의 돈 이다.
-
-// 	// 이 말은 blockchain이 아니라, 인간이 만드는 transaction을 발생시키고 싶다면,
-// 	// 인간이 트랜잭션의 출력값을 가지고 있어야 하고, 이 출력값을 트랜잭션 입력값으로 다시 변경해주어야 한다는 것을 말한다.
-// 	// transaction input은 예전의 transaction output 이다.
-// 	if Blockchain().BalanceByAddress(from) < amount {
-// 		return nil, errors.New("Not Enough money")
-// 	}
-
-// 	// 여러개의 TxOut 중에서 사용자가 주어야 하는 TxOut 만큼만 사용하면 된다.
-// 	// 목표는 크거나 같을 때 까지,
-// 	var txIns []*TxIn
-// 	var txOuts []*TxOut
-// 	total := 0
-// 	oldTxOuts := Blockchain().TxOutsByAddress(from)
-// 	for _, txOut := range oldTxOuts {
-// 		if total > amount {
-// 			break
-// 		}
-// 		txIn := &TxIn{txOut.Owner, txOut.Amount}
-// 		txIns = append(txIns, txIn)
-// 		total += txIn.Amount
-// 	}
-
-// 	// 잔돈이 있을 수 있기 때문에
-// 	change := total - amount
-// 	// 잔돈용 Tx 생성
-// 	if change != 0 {
-// 		changeTxOut := &TxOut{from, change}
-// 		txOuts = append(txOuts, changeTxOut)
-// 	}
-// 	// amount transaction output
-// 	txOut := &TxOut{to, amount}
-// 	txOuts = append(txOuts, txOut)
-
-// 	tx := &Tx{
-// 		Id:"",
-// 		Timestamp: int(time.Now().Unix()),
-// 		TxIns: txIns,
-// 		TxOuts: txOuts,
-// 	}
-// 	tx.getId()
-
-// 	return tx, nil
-// }
-
-// v2
 func makeTx(from, to string, amount int) (*Tx, error) {
 	if Blockchain().BalanceByAddress(from) < amount {
 		return nil, errors.New("not enough 돈")
@@ -171,8 +107,6 @@ func makeTx(from, to string, amount int) (*Tx, error) {
 
 }
 
-// 여기서 보내는 사람(from)은 필요하지 않다.
-// 보내는 사람은 function이 아닌, wallet에서 받아올 것이기 때문이다.
 func (m *mempool) AddTx(to string, amount int) error {
 	tx, err := makeTx("forme",to, amount)
 	if err != nil {
@@ -186,10 +120,6 @@ func (m *mempool) TxToConfirm() []*Tx {
 	coinbase := makeCoinbaseTx("forme")
 	txs := m.Txs
 	txs = append(txs, coinbase)
-
-	// memory pool 비우기
-	// mempool과 coinbase의 transaction들을 반환해 줬다는 의미
-	// 오로지, 블럭을 채굴했을 때만 발생하는 함수
 	m.Txs = nil
 	return txs
 }
