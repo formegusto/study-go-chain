d8a3d53f127b172456d8e0678192381f42c0045b
formegusto <formegusto@gmail.com>
2023-01-23 23:25:26 +0900

    P2P : ReadMessage, Welcome Message Proto

--- 08.p2p/p2p/p2p.go
+++ 08.p2p/p2p/p2p.go
@@ -3,7 +3,7 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"strings"
+	"time"
 
 	"github.com/formegusto/study-go-chain/utils"
 	"github.com/gorilla/websocket"
@@ -14,21 +14,26 @@ var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	// port :3000 will upgrade the request from :4000
-
+	address := utils.Splitter(r.RemoteAddr, ":", 0)
+	openPort := r.URL.Query().Get("openPort")
 	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
+		return openPort != "" && address != ""
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
 
-	address := strings.Split(r.RemoteAddr, ":")[0]
-	openPort := r.URL.Query().Get("openPort")
 	initPeer(conn, address, openPort)
+
+	time.Sleep(10 * time.Second)
+	conn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 3000!"))
 }
 
 func AddPeer(address, port, openPort string) {
 	// port :4000 is requesting an upgrade from the port :3000
-	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort), nil)
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
 	initPeer(conn, address, port)
+
+	time.Sleep(10 * time.Second)
+	conn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 4000!"))
 }
