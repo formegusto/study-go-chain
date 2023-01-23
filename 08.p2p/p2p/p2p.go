d5e49981ba3b0e2174094e037fd9eb89ced31110
formegusto <formegusto@gmail.com>
2023-01-23 23:40:36 +0900

    P2P : inbox, WriteMessage System

--- 08.p2p/p2p/p2p.go
+++ 08.p2p/p2p/p2p.go
@@ -22,18 +22,18 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
 
-	initPeer(conn, address, openPort)
+	peer := initPeer(conn, address, openPort)
 
 	time.Sleep(10 * time.Second)
-	conn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 3000!"))
+	peer.inbox <- []byte("Hello From 3000!")
 }
 
 func AddPeer(address, port, openPort string) {
 	// port :4000 is requesting an upgrade from the port :3000
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
-	initPeer(conn, address, port)
+	peer := initPeer(conn, address, port)
 
 	time.Sleep(10 * time.Second)
-	conn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 4000!"))
+	peer.inbox <- []byte("Hello From 4000!")
 }
