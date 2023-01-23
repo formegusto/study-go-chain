6af7e49681ccf35c69543d32577c57d8cf54fb1c
formegusto <formegusto@gmail.com>
2023-01-23 17:28:47 +0900

    Go Routine : Buffered Channel

--- 08.p2p/p2p.go
+++ 08.p2p/p2p.go
@@ -9,7 +9,7 @@ import (
 go routine basic
 */
 func BasicTest() {
-	c := make(chan int)
+	c := make(chan int, 5)
 	go countToTen(c)
 	receive(c)
 
@@ -20,21 +20,22 @@ func BasicTest() {
 
 func receive(c <-chan int) {
 	for {
+		time.Sleep(10 * time.Second)
 		a, ok := <-c
 		if !ok {
 			fmt.Println("We are done.")
 			break
 		}
-		fmt.Printf("received %d\n", a)
+		fmt.Printf("|| received %d ||\n", a)
 	}
 }
 
 func countToTen(c chan<- int) {
 	for i := range [10]int{} {
-		time.Sleep(1 * time.Second)
-		fmt.Printf("sending %d\n", i)
+		// time.Sleep(1 * time.Second)
+		fmt.Printf(">> sending %d <<\n", i)
 		c <- i
-		// fmt.Println(i)
+		fmt.Printf(">> sent %d <<\n", i)
 	}
 	close(c)
 }
