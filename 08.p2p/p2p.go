f0ad26c4ac5c089122745bbc52affc2999a80d0f
formegusto <formegusto@gmail.com>
2023-01-23 17:10:06 +0900

    Go Routine : Receive Function and Close

--- 08.p2p/p2p.go
+++ 08.p2p/p2p.go
@@ -10,24 +10,31 @@ go routine basic
 */
 func BasicTest() {
 	c := make(chan int)
-	go CountToTen(c)
-
-	fmt.Println("Blocking")
-	for {
-		a := <-c
-		fmt.Printf("received %d\n", a)
-	}
+	go countToTen(c)
+	receive(c)
 
 	// go CountToTen("two")
 	// *. channel 없이 기다리는 방법
 	// for {}
 }
 
-func CountToTen(c chan int) {
+func receive(c <-chan int) {
+	for {
+		a, ok := <-c
+		if !ok {
+			fmt.Println("We are done.")
+			break
+		}
+		fmt.Printf("received %d\n", a)
+	}
+}
+
+func countToTen(c chan<- int) {
 	for i := range [10]int{} {
 		time.Sleep(1 * time.Second)
 		fmt.Printf("sending %d\n", i)
 		c <- i
 		// fmt.Println(i)
 	}
+	close(c)
 }
