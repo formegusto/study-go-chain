8a0a9a4779b283a77af9ceffb0c058d7f4bf81e6
formegusto <formegusto@gmail.com>
2023-01-24 11:32:02 +0900

    P2P : Message Type

--- 08.p2p/p2p/peer.go
+++ 08.p2p/p2p/peer.go
@@ -51,11 +51,13 @@ func (p *peer) read() {
 	defer p.close()
 	// delete peer in case of error
 	for {
-		_, m, err := p.conn.ReadMessage()
+		// _, m, err := p.conn.ReadMessage()
+		m := Message{}
+		err := p.conn.ReadJSON(&m)
 		if err != nil {
 			break
 		}
-		fmt.Printf("%s\n", m)
+		fmt.Printf("%d %s\n", m.Kind, m.Payload)
 
 	}
 }
